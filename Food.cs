247f2aedcd2ace9d0245eb6715043028c4bf2e2a
Deraj103 <jstims9109@gmail.com>
2023-07-28 12:53:16 -0600

    Add project files.

---
+++ Food.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch7_Fix_It_Exercises
+{
+    internal class Food
+    {
+        private string name;
+        private double price;
+        static public double markup = 2;
+
+        //default constructor
+        public Food(string n, double p)
+        {
+            name = n;
+            price = p;
+        }
+
+        //getters
+        public string getName()
+        {
+            return name;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+
+        //setters
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch7_Fix_It_Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Ch7 Fix It Exercises by Jared Tims");
+
+            Write("\nPress any key to continue...");
+            ReadKey();
+        }
+    }
+}
